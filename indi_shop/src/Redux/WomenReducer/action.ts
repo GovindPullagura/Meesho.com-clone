29acce4bd27fc1e2370d2dc0a0dfd1785dc901a7
Govind <govind@Govinds-MacBook-Air.local>
2023-02-22 18:30:55 +0530

    redux for womenPage

---
+++ indi_shop/src/Redux/WomenReducer/action.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosResponse } from "axios";
+import { Dispatch } from "react";
+import { product } from "../../constants";
+import { DATA_FAIL, DATA_REQ, DATA_SUCCESS } from "../actionTypes";
+
+export const getReqAction = () => {
+  return { type: DATA_REQ };
+};
+
+export const getReqFailAction = () => {
+  return { type: DATA_FAIL };
+};
+
+export const getReqSuccessAction = (payload: product[]) => {
+  return { type: DATA_SUCCESS, payload };
+};
+
+// export const getWomenData = async () => {
+//   let res: AxiosResponse<product[]> = await axios.get(
+//     `https://indishop.onrender.com/women`
+//   );
+//   return res.data;
+// };
+export const getWomenData = (dispatch: Dispatch<any>) => {
+  dispatch(getReqAction());
+  axios
+    .get(`https://indishop.onrender.com/women`)
+    .then((res: AxiosResponse<product[]>) => {
+      console.log(res.data);
+      dispatch(getReqSuccessAction(res.data));
+    })
+    .catch((err) => dispatch(getReqFailAction()));
+};
+// dispatch(getReqSuccessAction(res.data))
