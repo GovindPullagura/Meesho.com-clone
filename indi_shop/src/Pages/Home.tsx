9f26e2a8ea731be572fd9e7266cdc35f3ce47428
Manju Sai <you@example.com>
2023-02-23 12:11:33 +0530

    nill

--- indi_shop/src/Pages/Home.tsx
+++ indi_shop/src/Pages/Home.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>;
-};
+  return (
+    <div>Home</div>
+  )
+}
 
-export default Home;
+export default Home
--- indi_shop/src/Pages/SignUpPage.tsx
+++ indi_shop/src/Pages/SignUpPage.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
-const SignUpPage = () => {
-  return <div>SignUpPage</div>;
-};
-
-export default SignUpPage;
+export const SignUpPage = () => {
+  return (
+    <div>SignUpPage</div>
+  )
+}
