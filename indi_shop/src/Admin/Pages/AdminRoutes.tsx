19e1ebf5ed6b77f29e09b1e614ad866e8dddb736
Vibhor Sharma <eminenceindia324@gmail.com>
2023-02-23 11:54:45 +0530

    day-2 commits-admin

---
+++ indi_shop/src/Admin/Pages/AdminRoutes.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+import { AddProduct } from "./Add Product/AddProduct";
+import { Dashboard } from "./Dashboard/Dashboard";
+import { Orders } from "./Orders/Orders";
+import { Products } from "./Products/Products";
+import { Users } from "./Users/Users";
+
+export const AdminRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+      <Outlet />
+    </>
+  );
+};
---
+++ indi_shop/src/Admin/Pages/Orders/Orders.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Orders = () => {
+  return <div>Orders</div>;
+};
