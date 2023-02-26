2c40db5af5ce85d1f09b727cf7ef194020e6b326
Govind <govind@Govinds-MacBook-Air.local>
2023-02-26 16:59:51 +0530

    day-5

--- indi_shop/src/Redux/HomeReducer/action.ts
+++ indi_shop/src/Redux/HomeReducer/action.ts
@@ -1,7 +1,13 @@
 import axios, { AxiosResponse } from "axios";
 import { Dispatch } from "react";
 import { axiosObj, product } from "../../constants";
-import { ADD_TO_CART, ALL_PRODUCT_FAILURE, ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS } from "./actionTypes";
+import {
+  ADD_TO_CART,
+  ALL_PRODUCT_FAILURE,
+  ALL_PRODUCT_REQUEST,
+  ALL_PRODUCT_SUCCESS,
+  GET_CART_DATA,
+} from "./actionTypes";
 
 export const getAllProductRequest = () => {
   return { type: ALL_PRODUCT_REQUEST };
@@ -18,18 +24,22 @@ export const getAllProductFailure = () => {
 export const addToCartAction = () => {
   return { type: ADD_TO_CART };
 };
-
-export const getAllProductData = (params: axiosObj) => (dispatch: Dispatch<any>) => {
-  // console.log(params);
-  dispatch(getAllProductRequest());
-  axios
-    .get(`https://indishop.onrender.com/products`, params)
-    .then((res: AxiosResponse<product[]>) => {
-      // console.log(res.data);
-      dispatch(getAllProductSuccess(res.data));
-    })
-    .catch((err) => dispatch(getAllProductFailure()));
+export const getCartDataSuccess = (payload: product[]) => {
+  return { type: GET_CART_DATA, payload };
 };
+
+export const getAllProductData =
+  (params: axiosObj) => (dispatch: Dispatch<any>) => {
+    // console.log(params);
+    dispatch(getAllProductRequest());
+    axios
+      .get(`https://indishop.onrender.com/products`, params)
+      .then((res: AxiosResponse<product[]>) => {
+        // console.log(res.data);
+        dispatch(getAllProductSuccess(res.data));
+      })
+      .catch((err) => dispatch(getAllProductFailure()));
+  };
 
 export const addToCart = (data: product) => (dispatch: Dispatch<any>) => {
   dispatch(getAllProductRequest());
@@ -37,4 +47,11 @@ export const addToCart = (data: product) => (dispatch: Dispatch<any>) => {
     .post(`https://indishop.onrender.com/cart`, data)
     .then((res) => dispatch(addToCartAction()))
     .catch((err) => dispatch(getAllProductFailure()));
+};
+export const getcartData = (dispatch: Dispatch<any>) => {
+  axios
+    .get(`https://indishop.onrender.com/cart`)
+    .then((res: AxiosResponse<product[]>) => {
+      dispatch(getCartDataSuccess(res.data));
+    });
 };
